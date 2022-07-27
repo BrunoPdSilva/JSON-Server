33a077cc95f38ca9c3eab110aa5c2ec1066c1e1c
Bruno Peres <bruno_16515@hotmail.com>
2022-07-27 10:51:50 -0300

    buscando dados e renderizando

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // javascript for index.html
+const container = document.querySelector('.blogs')
+
+const renderPosts = async () => {
+  let uri = "http://localhost:3000/posts";
+
+  const res = await fetch(uri);
+  const posts = await res.json();
+
+  let template = "";
+  posts.forEach(post => {
+    template += `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0, 200)}</p>
+        <a href="/details.html?id=${post.id}">read more...</a>
+      </div>`;
+  });
+
+  container.innerHTML = template;
+
+};
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
